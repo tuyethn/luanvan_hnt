c89aa70655edf1f9988dce5e83b7c3329710dad2
tuyethn <hntuyet1003@gmail.com>
2024-10-07 15:27:06 +0700

    Up file haha

---
+++ haha.py
@@ -0,0 +1,4 @@
+# Python 3.12.4 | packaged by Anaconda, Inc. | (main, Jun 18 2024, 15:03:56) [MSC v.1929 64 bit (AMD64)] on win32
+# Type "help", "copyright", "credits" or "license()" for more information.
+print('hello')
+# hello
