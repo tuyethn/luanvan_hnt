46eca38e1217747c4c7392c973936f96beb3017f
tuyethn <158500745+tuyethn@users.noreply.github.com>
2024-10-07 15:32:10 +0700

    Update haha.py

--- haha.py
+++ haha.py
@@ -1,4 +1,5 @@
 # Python 3.12.4 | packaged by Anaconda, Inc. | (main, Jun 18 2024, 15:03:56) [MSC v.1929 64 bit (AMD64)] on win32
 # Type "help", "copyright", "credits" or "license()" for more information.
 print('hello')
+print('No one know me')
 # hello
